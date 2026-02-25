18d171c0bdcc95f18a69ed1b9d7bbfcf63b3dc6c
Gabriel Vian <gvian07@gmail.com>
2026-02-25 15:45:43 -0800

    Remove leftover console.logs in fix-dark-mode.js

--- src/fix-dark-mode.js
+++ src/fix-dark-mode.js
@@ -1,6 +1,5 @@
 function fixDarkMode() {
 	const darkModeEnabled = document.querySelector('html').classList.contains('dark');
-	console.log('dark mode is', darkModeEnabled);
 
 	if (window.deSaffSettings.fixDarkModeText && darkModeEnabled) {
 		// 1. Grab only elements that have an inline style attribute
@@ -14,13 +13,11 @@ function fixDarkMode() {
 
 			// 3. Check if the parsed setting matches any variation of black
 			if (textColor === 'rgb(0,0,0)' || textColor === '#000000' || textColor === 'black' || textColor === '#000') {
-				console.log('found font color:', el.innerText);
 				el.style.color = 'rgb(207, 208, 209)';
 			}
 
 			// 4. Check if the parsed setting matches any variation of white
 			if (bgColor === 'rgb(255,255,255)' || bgColor === '#ffffff' || bgColor === 'white' || bgColor === '#fff') {
-				console.log('found background color:', el.innerText);
 				el.style.backgroundColor = 'transparent'; // 'none' is invalid CSS
 			}
 		}
